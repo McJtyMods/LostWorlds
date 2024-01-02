5680d7a3ae933b5ab5b00757d9e3d37145ee5353
McJty <mcjty1@gmail.com>
2024-01-02 06:47:33 +0100

    Small changes

--- src/main/java/mcjty/lostworlds/worldgen/FogColor.java
+++ src/main/java/mcjty/lostworlds/worldgen/FogColor.java
@@ -7,8 +7,9 @@ public enum FogColor implements StringRepresentable {
     NONE("None", null),
     BLACK("Black", new Vec3(0, 0, 0)),
     GRAY("Gray", new Vec3(0.5, 0.5, 0.5)),
-    RED("Red", new Vec3(0.8, 0.4, 0.4)),
-    BLUE("Blue", new Vec3(0.4, 0.4, 0.8));
+    RED("Red", new Vec3(0.8, 0.2, 0.2)),
+    BLUE("Blue", new Vec3(0.4, 0.4, 0.8)),
+    YELLOW("Yellow", new Vec3(0.8, 0.8, 0.4));
 
     private final String name;
     private final Vec3 fogColor;
